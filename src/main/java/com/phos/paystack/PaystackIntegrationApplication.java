8930b00458946da64a8899cf3bc6ca672aee22b7
Adefolarin Osu <aosu@teamapt.com>
2020-08-21 13:54:30 +0100

    first commit

---
+++ src/main/java/com/phos/paystack/PaystackIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.phos.paystack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystackIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaystackIntegrationApplication.class, args);
+	}
+
+}
